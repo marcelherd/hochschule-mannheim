1373cd7c6ebad016fdc61c439928a8db63c45a36
Marcel Herd <me@marcelherd.com>
2015-10-15 15:05:00 +0200

    Updated Uebung 3 Aufgabe 1

--- Workspace/GDI/src/com/marcelherd/uebung3/ISBNBerechnung.java
+++ Workspace/GDI/src/com/marcelherd/uebung3/ISBNBerechnung.java
@@ -2,6 +2,8 @@ package com.marcelherd.uebung3;
 
 import static gdi.MakeItSimple.*;
 
+import java.util.Arrays;
+
 import gdi.MISException;
 
 /**
@@ -19,8 +21,33 @@ import gdi.MISException;
 public class ISBNBerechnung {
 
 	public static void main(String[] args) {
-		print("Bitte geben Sie die ISBN-13 (ohne Pruefziffer) ein: ");
-		int isbn = readInt();
+		print("Bitte geben Sie die ISBN-13 (ohne Pruefziffer und Bindestriche) ein: ");
+		String isbn = readLine();
+		
+		char[] chars = isbn.toCharArray();
+		int[] zahlen = new int[12];
+		for (int i = 0; i < chars.length; i++) {
+			zahlen[i] = Integer.valueOf(chars[i] + "");
+		}
+		System.out.println(Arrays.toString(zahlen));
+		
+		int summe = 0;
+		for (int i = 0; i < zahlen.length; i++) {
+			if ((i+1) % 2 == 0) {
+				summe += zahlen[i] * 3;
+			} else {
+				summe += zahlen[i];
+			}
+		}
+		
+		String summeString = summe + "";
+		int letzteZahl = Integer.valueOf(summeString.charAt(summeString.length() - 1) + "");
+		
+		int pruefziffer = 0;
+		if (summe != 10) {
+			pruefziffer = 10 - letzteZahl;
+		}
+		println(pruefziffer);
 	}
 
 }
