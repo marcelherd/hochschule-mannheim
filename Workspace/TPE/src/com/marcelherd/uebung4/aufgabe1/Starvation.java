3a9f7e8380e96e7fb6a4137a400c34d81df8c59b
Marcel Herd <me@marcelherd.com>
2016-06-06 20:12:41 +0200

    Aufgabe 1 fertig

--- Workspace/TPE/src/com/marcelherd/uebung4/aufgabe1/Starvation.java
+++ Workspace/TPE/src/com/marcelherd/uebung4/aufgabe1/Starvation.java
@@ -6,23 +6,33 @@ public class Starvation {
 	protected volatile boolean active = true;
 
 	public Starvation() {
-		Foo[] threads = new Foo[1000];
-		threads[0] = new Foo(this, "Thread 1", Thread.MAX_PRIORITY);
-		for (int i = 1; i < threads.length - 1; i++) {
+		System.out.println("Creating 100.000 Threads. This will take a while.");
+		
+		Foo[] threads = new Foo[100000];
+		for (int i = 0; i < threads.length - 1; i++) { // 99999 Threads haben maximale Priorität
 			threads[i] = new Foo(this, "Thread " + (i + 1), Thread.MAX_PRIORITY);
 		}
-		threads[threads.length - 1] = new Foo(this, "Thread 1000", Thread.MIN_PRIORITY);
+		threads[threads.length - 1] = new Foo(this, "Thread 1000", Thread.MIN_PRIORITY); // der letzte Thread hat minimale Priorität
 		
 		for (Thread thread : threads) thread.start();
 		
-		work(3000);
+		System.out.print("Working");
+		for (int i = 0; i < 10; i++) {
+			work(500);
+			System.out.print(".");
+		}
+		
+		System.out.println();
 		
 		active = false;
+
+		int sum = 0; // Durchschnittscounter berechnen
+		for (Foo thread : threads) {
+			sum += thread.counter;
+		}
 		
-		work(500);
-		
-		System.out.println("----------------------------------------");
-		System.out.println("MAX PRIORITY: " + threads[0]);
+		System.out.println("----------------------------------------------");
+		System.out.println("AVERAGE: counter=" + (sum / threads.length));
 		System.out.println("MIN PRIORITY: " + threads[threads.length - 1]);
 	}
 	
@@ -41,7 +51,7 @@ public class Starvation {
 		
 		private Starvation holder;
 
-		private long counter = 0;
+		protected long counter = 0;
 
 		public Foo(Starvation holder, String name, int priority) {
 			super(name);
@@ -51,28 +61,34 @@ public class Starvation {
 
 		@Override
 		public void run() {
-			while (holder.active) {
-				synchronized (holder.sharedResource) {
-					step();
+			while (holder.active) { // solange das Programm läuft
+				synchronized (holder.sharedResource) { // versuche die Resource zu locken
+					step(); // tu etwas
 				}
 			}
-			System.out.println("Final state: " + this);
 		}
 		
 		private void step() {
 			counter++;
 		}
-		
+
+		@Override
 		public String toString() {
-			return "Current Thread: " + Thread.currentThread().getName() + ", counter: " + counter + ", prio: " + getPriority();
+			StringBuilder builder = new StringBuilder();
+			builder.append("Foo [counter=");
+			builder.append(counter);
+			builder.append(", getPriority()=");
+			builder.append(getPriority());
+			builder.append(", getName()=");
+			builder.append(getName());
+			builder.append("]");
+			return builder.toString();
 		}
 
 	}
 	
 	public static void main(String[] args) {
 		new Starvation();
-		
-		// Well, this doesn't exactly work. I guess its because Windows is interfering.
 	}
 
 }
